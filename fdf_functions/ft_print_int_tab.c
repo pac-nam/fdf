6ccf2d8307302b26d3ad712257346f66cb03fb05
Thomas BLEUSE <tbleuse@student.42.fr>
2018-03-29 16:59:30 +0200

    changed print_int_tab

--- fdf_functions/ft_print_int_tab.c
+++ fdf_functions/ft_print_int_tab.c
@@ -6,7 +6,7 @@
 /*   By: tbleuse <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/29 16:41:03 by tbleuse           #+#    #+#             */
-/*   Updated: 2018/03/29 16:51:34 by tbleuse          ###   ########.fr       */
+/*   Updated: 2018/03/29 16:59:00 by tbleuse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,13 @@ int		ft_print_int_tab(int **map)
 	int		x;
 
 	y = 1;
-	ft_putnbr(map[0][0]);
+	ft_printf("%d", map[0][0]);
 	while (y <= map[0][0])
 	{
 		ft_putchar('\n');
 		x = 0;
 		while (x <= map[y][0])
-			ft_printf("%d	", map[y][x++]);
+			ft_printf("%4d", map[y][x++]);
 		++y;
 	}
 	ft_putchar('\n');
