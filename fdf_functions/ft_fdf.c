c9f7c536d5a0a2d2b88f3cd6c92160b0c122651c
Thomas BLEUSE <tbleuse@student.42.fr>
2018-04-24 08:22:42 +0200

    almost finished

--- fdf_functions/ft_fdf.c
+++ fdf_functions/ft_fdf.c
@@ -6,13 +6,31 @@
 /*   By: tbleuse <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/30 13:46:17 by tbleuse           #+#    #+#             */
-/*   Updated: 2018/04/23 15:34:20 by tbleuse          ###   ########.fr       */
+/*   Updated: 2018/04/23 15:47:44 by tbleuse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/fdf.h"
 
-int		ft_write_map(t_point *p1, t_point *p2, t_stock *s)
+static void	ft_write_map_second(t_point *p1, t_point *p2, t_stock *s, int xy[])
+{
+	if (xy[1] < s->map[xy[0]][0])
+	{
+		p2->x = s->ref->x + (xy[1]) * s->dif_right_x;
+		p2->y = s->ref->y - (xy[1]) * s->dif_right_y -
+			s->map[xy[0]][xy[1] + 1] * s->zoom;
+		ft_put_line(p1, p2, s);
+	}
+	if (xy[0] < s->map[0][0] && xy[1] <= s->map[xy[0] + 1][0])
+	{
+		p2->x = p1->x + s->dif_up_x;
+		p2->y = s->ref->y - (xy[1] - 1) * s->dif_right_y + s->dif_up_y -
+			s->map[xy[0] + 1][xy[1]] * s->zoom;
+		ft_put_line(p1, p2, s);
+	}
+}
+
+int			ft_write_map(t_point *p1, t_point *p2, t_stock *s)
 {
 	int	xy[2];
 
@@ -25,20 +43,7 @@ int		ft_write_map(t_point *p1, t_point *p2, t_stock *s)
 			p1->x = s->ref->x + (xy[1] - 1) * s->dif_right_x;
 			p1->y = s->ref->y - (xy[1] - 1) * s->dif_right_y -
 				s->map[xy[0]][xy[1]] * s->zoom;
-			if (xy[1] < s->map[xy[0]][0])
-			{
-				p2->x = s->ref->x + (xy[1]) * s->dif_right_x;
-				p2->y = s->ref->y - (xy[1]) * s->dif_right_y -
-					s->map[xy[0]][xy[1] + 1] * s->zoom;
-				ft_put_line(p1, p2, s);
-			}
-			if (xy[0] < s->map[0][0] && xy[1] <= s->map[xy[0] + 1][0])
-			{
-				p2->x = p1->x + s->dif_up_x;
-				p2->y = s->ref->y - (xy[1] - 1) * s->dif_right_y + s->dif_up_y -
-					s->map[xy[0] + 1][xy[1]] * s->zoom;
-				ft_put_line(p1, p2, s);
-			}
+			ft_write_map_second(p1, p2, s, xy);
 		}
 		s->ref->x += s->dif_up_x;
 		s->ref->y += s->dif_up_y;
@@ -46,7 +51,7 @@ int		ft_write_map(t_point *p1, t_point *p2, t_stock *s)
 	return (1);
 }
 
-int		ft_fdf(t_stock *s)
+int			ft_fdf(t_stock *s)
 {
 	t_point		*p1;
 	t_point		*p2;
